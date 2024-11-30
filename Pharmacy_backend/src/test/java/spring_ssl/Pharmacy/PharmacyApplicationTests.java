f30d201befc3a88e7e282be244d708d0a13f41b5
tomkat44 <tomkatsantas@gmail.com>
2024-11-30 17:15:02 +0200

    Initial backend push

---
+++ Pharmacy_backend/src/test/java/spring_ssl/Pharmacy/PharmacyApplicationTests.java
@@ -0,0 +1,13 @@
+package spring_ssl.Pharmacy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmacyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
