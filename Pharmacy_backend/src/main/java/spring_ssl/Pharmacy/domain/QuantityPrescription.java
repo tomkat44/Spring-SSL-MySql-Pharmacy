64bb242cfc74b93760af5443d94f9c93fccc148c
tomkat44 <tomkatsantas@gmail.com>
2024-12-04 17:46:51 +0200

    Quantity Prescription not workinh NullPointerException

--- Pharmacy_backend/src/main/java/spring_ssl/Pharmacy/domain/QuantityPrescription.java
+++ Pharmacy_backend/src/main/java/spring_ssl/Pharmacy/domain/QuantityPrescription.java
@@ -8,7 +8,7 @@ public class QuantityPrescription {
 
     @Id
     @Column(name="Id", nullable = false)
-    @GeneratedValue(strategy = GenerationType.IDENTITYgit )
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     @Column(name="quantityPrescription", length = 10, nullable = false)
